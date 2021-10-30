dad75f8ef6ad4bb6b711a7a833b9d1ffaed55a5a
Azryel <AzryelShroud@gmail.com>
2021-10-30 18:13:42 +0200

    connect to db

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const app = express()
 const http = require("http")
 const server = http.createServer(app)
 
+const mysql = require("mysql2")
+
+require('dotenv').config()
+
 const cors = require("cors")
 
 app.use(cors())
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
+
+connection.connect( (err) => {
+    if (err) throw err
+
+    console.log("Connected to database.")
+})
 
 app.get("/", (req, res) =>  {
     res.send("Welcome")
