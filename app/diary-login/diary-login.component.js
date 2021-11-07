0b959ed8ed8d2be3e0ba3e056287913be3e9481c
Azryel <AzryelShroud@gmail.com>
2021-11-07 18:13:06 +0100

    puc

--- app/diary-login/diary-login.component.js
+++ app/diary-login/diary-login.component.js
@@ -12,51 +12,46 @@ angular.
         self.password = ""
         self.confirmpassword = ""
 
+
+
         self.login = () => {
 
           $http.post('http://localhost:3000/login',
             {
-                username: self.username,
-                password: self.password
+              username: self.username,
+              password: self.password
             }
             ).then( (res) => {
-              console.log(res)
-              console.log("TOKEN", res.data.token)
-              console.log("USER", res.data.user)
 
               localStorage.setItem("token", res.data.token)
               localStorage.setItem("user", res.data.user)
               $window.location.reload()
             }, (err) => {
               console.log( "ERROR", err.data)
-
-            } )
+            })
           }
 
-          self.create = () => {
-            console.log("CRE")
-            if( self.password !== self.confirmpassword ) {
-              console.log("SAME")
-              return alert("Passwords dont match")
+        self.create = () => {
+
+          if( self.password !== self.confirmpassword ) {
+            return alert("Passwords dont match")
+          }
+
+          $http.post('http://localhost:3000/users',
+            {
+                username: self.username,
+                password: self.password
             }
+            ).then( (res) => {
 
-            $http.post('http://localhost:3000/users',
-              {
-                  username: self.username,
-                  password: self.password
-              }
-              ).then( (res) => {
-                console.log(res)
+              self.confirmpassword = ""
 
-                self.username = ""
-                self.password = ""
-                self.confirmpassword = ""
+              self.login()
 
-              }, (err) => {
-                console.log(err.data)
-
-              } )
-            }
+            }, (err) => {
+              console.log( "ERROR", err.data)
+            })
+          }
 
 
         }
--- app/diary-user/diary-user.component.js
+++ app/diary-user/diary-user.component.js
@@ -31,109 +31,99 @@ angular.
 
         self.entryText = ""
 
+        self.errors = [ ]
+
         self.opneEntry = (entry) => {
           self.singleEntry = entry
           self.diarySection = "singleEntry"
         }
 
-        $http.get("http://localhost:3000/diary-entries",
-              {
-                  headers: {
-                    Authorization: `Bearer ` +  localStorage.getItem("token")
-                  }
-              }
-              ).then( (res) => {
-                console.log(" ENTRIES ", res.data)
-                self.entries = res.data
-              }, (err) => {
-                console.log(err.data)
-
-              } )
-        
-        $http.get("http://localhost:3000/profiles",
-              {
-                  headers: {
-                    Authorization: `Bearer ` +  localStorage.getItem("token")
-                  }
-              }
-              ).then( (res) => {
-                console.log(" PROFILE ", res.data[0])
-                self.profile = res.data[0]
-                self.profileUP = { ... self.profile }
-              }, (err) => {
-                console.log(err.data)
-
-              } )
-          
-              self.changeTheme = (  ) => {
-                /*
-                console.log("theme", getComputedStyle(document.documentElement)
-                  .getPropertyValue("--theme") )
-                */
-                document.documentElement.style
-                  .setProperty('--theme', self.theme)
-
-
+        self.changeTheme = (  ) => {
+          /*
+          console.log("theme", getComputedStyle(document.documentElement)
+            .getPropertyValue("--theme") )
+          */
+          document.documentElement.style
+            .setProperty('--theme', self.theme)
         }
 
+
+        // http
+
+        $http.get("http://localhost:3000/diary-entries",
+          {
+              headers: {
+                Authorization: `Bearer ` +  localStorage.getItem("token")
+              }
+          }
+          ).then( (res) => {
+            self.entries = res.data
+          }, (err) => {
+            console.log(err.data)
+          })
+        
+        $http.get("http://localhost:3000/profiles",
+          {
+              headers: {
+                Authorization: `Bearer ` +  localStorage.getItem("token")
+              }
+          }
+          ).then( (res) => {
+            self.profile = res.data[0]
+            self.profileUP = { ... self.profile }
+          }, (err) => {
+            console.log("ERROR", err.data)
+          })
+
         self.updateProfile = () => {
-          console.log("UPDATE")
-          console.log(localStorage.getItem("token"))
           $http(
               {
-                  method: "PUT",
-                  url: "http://localhost:3000/profiles/" + self.profile.userID ,
-                  data: {
-                    ... self.profileUP
-                  },
-              
-                  headers: {
-                    Authorization: `Bearer ` +  localStorage.getItem("token")
-                  }
+                method: "PUT",
+                url: "http://localhost:3000/profiles/" + self.profile.userID ,
+                data: {
+                  ... self.profileUP
+                },
+                headers: {
+                  Authorization: `Bearer ` +  localStorage.getItem("token")
+                }
               }
               ).then( (res) => {
 
                 if(res.data.message === "Profile updated") {
-                  console.log(" PROFILE UP", res)
 
                   self.profile = { ... self.profileUP }
                   self.diarySection = "profile"
 
                 }
               }, (err) => {
-                console.log(err.data)
-
-              } )
+                console.log("ERROR", err.data)
+              })
         }
 
 
         self.createProfile = () => {
-          console.log("CREATE")
           $http(
               {
-                  method: "POST",
-                  url: "http://localhost:3000/profiles/",
-                  data: {
-                    ... self.profileUP
-                  },
-              
-                  headers: {
-                    Authorization: `Bearer ` +  localStorage.getItem("token")
-                  }
+                method: "POST",
+                url: "http://localhost:3000/profiles/",
+                data: {
+                  ... self.profileUP
+                },
+                headers: {
+                  Authorization: `Bearer ` +  localStorage.getItem("token")
+                }
               }
               ).then( (res) => {
 
                 if(res.data.message === "Profile posted") {
-                  console.log(" PROFILE UP", res)
 
                   self.profile = { ... self.profileUP }
                   self.diarySection = "profile"
 
                 }
               }, (err) => {
-                console.log(err.data)
-
-              } )
+                console.log("ERROR", err.data)
+              })
         }
 
 
@@ -142,20 +132,18 @@ angular.
 
           $http(
             {
-                method: "POST",
-                url: "http://localhost:3000/diary-entries/" ,
-                data: {
-                  date: self.currentDate,
-                  text: self.entryText
-                },
-            
-                headers: {
-                  Authorization: `Bearer ` +  localStorage.getItem("token")
-                }
+              method: "POST",
+              url: "http://localhost:3000/diary-entries/" ,
+              data: {
+                date: self.currentDate,
+                text: self.entryText
+              },
+              headers: {
+                Authorization: `Bearer ` +  localStorage.getItem("token")
+              }
             }
             ).then( (res) => {
 
-                console.log(" NEW ENTRY ", res)
                 self.entries.push({
                   date: self.currentDate,
                   text: self.entryText
@@ -164,12 +152,10 @@ angular.
 
             }, (err) => {
               console.log("ERROR",err.data)
-
-            } )
+            })
         }
 
         self.deleteEntry = (entryID) => {
-          console.log("delete ENTRY", entryID)
 
           const confirmDelete = confirm("Confirm delete?")
 
@@ -179,28 +165,25 @@ angular.
 
           $http(
             {
-                method: "DELETE",
-                url: "http://localhost:3000/diary-entries/" + entryID ,
-                headers: {
-                  Authorization: `Bearer ` +  localStorage.getItem("token")
-                }
+              method: "DELETE",
+              url: "http://localhost:3000/diary-entries/" + entryID,
+              headers: {
+                Authorization: `Bearer ` +  localStorage.getItem("token")
+              }
             }
             ).then( (res) => {
 
-                console.log(" DELETED ENRTY ", res)
                 self.entries = self.entries.filter( entry => entry.entryID !== entryID )
                 self.diarySection = "entries"
 
             }, (err) => {
               console.log("ERROR",err.data)
-
-            } )
+            })
         }
 
 
 
         self.deleteAccount = () => {
-          console.log("DEL", localStorage.getItem("user"))
 
           const confirmDelete = confirm("Confirm delete?")
 
@@ -211,28 +194,20 @@ angular.
 
           $http(
             {
-                method: "DELETE",
-                url: "http://localhost:3000/users/" + localStorage.getItem("user") ,
-                headers: {
-                  Authorization: `Bearer ` +  localStorage.getItem("token")
-                }
+              method: "DELETE",
+              url: "http://localhost:3000/users/" + localStorage.getItem("user") ,
+              headers: {
+                Authorization: `Bearer ` +  localStorage.getItem("token")
+              }
             }
             ).then( (res) => {
-
-                console.log(" DELETE ", res)
-
                 self.logout()
-
-
             }, (err) => {
               console.log("ERROR",err.data)
+            })
 
-            } )
 
         }
-
-
       }
     ]
-  
   })
